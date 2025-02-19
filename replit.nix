2320579090e46a4c67692e8b83cba68c1ef880a0
romiluz13 <rom@iluz.net>
2025-02-19 16:13:23 +0200

    chore: add Replit configuration for proper deployment

--- replit.nix
+++ replit.nix
@@ -1,8 +1,9 @@
-{ pkgs }: {
-    deps = [
-        pkgs.nodejs-18_x
-        pkgs.nodePackages.typescript-language-server
-        pkgs.nodePackages.npm
-        pkgs.nodePackages.typescript
-    ];
+{
+  description = "Hadassa & Oscar RSVP App";
+  deps = [
+    pkgs.nodejs-18_x
+    pkgs.nodePackages.typescript-language-server
+    pkgs.yarn
+    pkgs.replitPackages.jest
+  ];
 } 
